e9eb650fd52ec4188ceb72dfb4cd1d909350fcfa
AC Jacinto <jacintoalydacaye@gmail.com>
2023-11-20 15:47:15 +0800

    Added Blog Pages

---
+++ src/pages/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">About</h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Culpa in aliquip dolor anim proident sint quis quis irure consectetur
+        sit in. Commodo sit ad ipsum esse aliqua qui sunt consequat excepteur
+        sit velit. Est tempor ullamco magna enim sint labore irure quis velit.
+        Id incididunt duis minim minim aliqua proident ad culpa cillum in anim
+        est excepteur aliqua.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Culpa in aliquip dolor anim proident sint quis quis irure consectetur
+        sit in. Commodo sit ad ipsum esse aliqua qui sunt consequat excepteur
+        sit velit. Est tempor ullamco magna enim sint labore irure quis velit.
+        Id incididunt duis minim minim aliqua proident ad culpa cillum in anim
+        est excepteur aliqua.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Culpa in aliquip dolor anim proident sint quis quis irure consectetur
+        sit in. Commodo sit ad ipsum esse aliqua qui sunt consequat excepteur
+        sit velit. Est tempor ullamco magna enim sint labore irure quis velit.
+        Id incididunt duis minim minim aliqua proident ad culpa cillum in anim
+        est excepteur aliqua. Culpa in aliquip dolor anim proident sint quis
+        quis irure consectetur sit in. Commodo sit ad ipsum esse aliqua qui sunt
+        consequat excepteur sit velit. Est tempor ullamco magna enim sint labore
+        irure quis velit. Id incididunt duis minim minim aliqua proident ad
+        culpa cillum in anim est excepteur aliqua.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Article.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Article = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">This is an Article</h1>
+    </div>
+  );
+};
+
+export default Article;
